e16b5953c8b6e012daa31809ef846cc384242724
Ivas_ee78 <Ivas_ee78@student.itstep.org>
2023-04-08 16:32:33 +0300

    home1

---
+++ home1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace home1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
